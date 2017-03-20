ea6acc11e7b4168457cc6a71e537052dabb3240b
Dylansweaver <dylansweaver@gmail.com>
2017-03-20 01:33:41 -0500

    Update ViewController.swift

--- MotorcycleApp/ViewController.swift
+++ MotorcycleApp/ViewController.swift
@@ -20,6 +20,23 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+extension ViewController : CLLocationManagerDelegate {
+        func locationManager(manager: CLLocationManager, didChangeAuthorizationStatus status: CLAuthorizationStatus) {
+            if status == .AuthorizedWhenInUse {
+                locationManager.requestLocation()
+            }
+        }
+        
+        func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+            if let location = locations.first {
+                print("location:: (location)")
+            }
+        }
+        
+        func locationManager(manager: CLLocationManager, didFailWithError error: NSError) {
+            print("error:: (error)")
+        }
+    }
 
 }
 
