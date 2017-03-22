bfd7702ac80fe478ebebd367d1c078052f1cf899
Dylansweaver <dylansweaver@gmail.com>
2017-03-22 15:29:12 -0500

    Search Bar Working

--- MotorcycleApp/ViewController.swift
+++ MotorcycleApp/ViewController.swift
@@ -7,9 +7,17 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate {
 
+   
+    @IBOutlet weak var textSearch: UITextField!
+    @IBOutlet weak var mapView: MKMapView!
+    var matchingItems: [MKMapItem] = [MKMapItem]()
+    
+    
+   
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +27,44 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
+    @IBAction func textFieldReturn(_ sender: AnyObject) {
+        _ = sender.resignFirstResponder()
+        mapView.removeAnnotations(mapView.annotations)
+        self.performSearch()
+    }
+    func performSearch() {
+        
+        matchingItems.removeAll()
+        let request = MKLocalSearchRequest()
+        request.naturalLanguageQuery = textSearch.text
+        request.region = mapView.region
+        
+        let search = MKLocalSearch(request: request)
+        
+        search.start(completionHandler: {(response, error) in
+            
+            if error != nil {
+                print("Error occured in search: \(error!.localizedDescription)")
+            } else if response!.mapItems.count == 0 {
+                print("No matches found")
+            } else {
+                print("Matches found")
+                
+                for item in response!.mapItems {
+                    print("Name = \(item.name)")
+                    print("Phone = \(item.phoneNumber)")
+                    
+                    self.matchingItems.append(item as MKMapItem)
+                    print("Matching items = \(self.matchingItems.count)")
+                    
+                    let annotation = MKPointAnnotation()
+                    annotation.coordinate = item.placemark.coordinate
+                    annotation.title = item.name
+                    self.mapView.addAnnotation(annotation)
+                }
+            }
+        })
+    }
 }
-
